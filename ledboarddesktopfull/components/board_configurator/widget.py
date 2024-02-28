cf495ad9b4d87e83437daaf02e4bcbc717232a88
MrFrangipane <contact@frangipane.me>
2024-02-28 15:05:00 +0100

    feat(Configurator): display read only infos

--- ledboarddesktopfull/components/board_configurator/widget.py
+++ ledboarddesktopfull/components/board_configurator/widget.py
@@ -30,6 +30,10 @@ class BoardConfiguratorWidget(QWidget):
         self.button_apply.setIcon(icons.play_button())
         self.button_apply.clicked.connect(self.apply)
 
+        self.label_hardware_revision = QLabel()
+        self.label_hardware_id = QLabel()
+        self.label_firmware_revision = QLabel()
+
         self.button_save_and_reboot = QPushButton("Save and reboot")
         self.button_save_and_reboot.setIcon(icons.diskette())
         self.button_save_and_reboot.clicked.connect(self.save_and_reboot)
@@ -48,16 +52,28 @@ class BoardConfiguratorWidget(QWidget):
         layout.addWidget(QLabel("Pixel Type"), 4, 0)
         layout.addWidget(self.combo_pixel_type, 4, 1)
 
-        layout.addWidget(self.button_apply, 5, 0, 1, 2)
-        layout.addWidget(self.button_save_and_reboot, 6, 0, 1, 2)
+        layout.addWidget(QLabel("Firmware revision"), 5, 0)
+        layout.addWidget(self.label_firmware_revision, 5, 1)
+        layout.addWidget(QLabel("Hardware revision"), 6, 0)
+        layout.addWidget(self.label_hardware_revision, 6, 1)
+        layout.addWidget(QLabel("Hardware ID"), 7, 0)
+        layout.addWidget(self.label_hardware_id, 7, 1)
+
+        layout.addWidget(self.button_apply, 8, 0, 1, 2)
+        layout.addWidget(self.button_save_and_reboot, 9, 0, 1, 2)
 
     def refresh(self):
         configuration = Components().board_api.get_configuration()
+
         self.line_name.setText(configuration.name.strip())
         self.line_ip_address.setText(str(configuration.ip_address))
         self.spin_universe.setValue(configuration.universe)
         self.spin_pixels_per_transmitter.setValue(configuration.pixel_per_transmitter)
         self.combo_pixel_type.setCurrentIndex(configuration.pixel_type.value)
+
+        self.label_firmware_revision.setText(str(configuration.firmware_revision))
+        self.label_hardware_id.setText(configuration.hardware_id)
+        self.label_hardware_revision.setText(str(configuration.hardware_revision))
 
     def save_and_reboot(self):
         self._send(save=True)
